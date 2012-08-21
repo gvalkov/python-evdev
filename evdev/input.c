f8a4a195dee3e095fca04bef9a47fe6928fa48ef
Georgi Valkov <georgi.t.valkov@gmail.com>
2012-08-22 00:28:42 +0300

    factor capability query functionality out of ioctl_devinfo

--- evdev/input.c
+++ evdev/input.c
@@ -122,7 +122,58 @@ event_unpack(PyObject *self, PyObject *args)
 
     memcpy(&event, data, sizeof(event));
 
-    return Py_BuildValue("i", 1);
+    Py_RETURN_NONE;
+}
+
+
+// Get the event types and event codes that the input device supports
+static PyObject *
+ioctl_capabilities(PyObject *self, PyObject *args)
+{
+    int fd, i, j;
+    char ev_bits[EV_MAX/8], code_bits[KEY_MAX/8];
+
+    int ret = PyArg_ParseTuple(args, "i", &fd);
+    if (!ret) return NULL;
+
+    // @todo: figure out why fd gets zeroed on the ioctls after the
+    // refactoring and get rid of this workaround
+    const int _fd = fd;
+
+    // Capabilities is a mapping of supported event types to lists of handled
+    // evenes e.g: {1: [272, 273, 274, 275], 2: [0, 1, 6, 8]}
+    PyObject* capabilities = PyDict_New();
+    PyObject* eventcodes = NULL;
+    PyObject* capability = NULL;
+
+    memset(&ev_bits, 0, sizeof(ev_bits));
+
+    if (ioctl(_fd, EVIOCGBIT(0, EV_MAX), ev_bits) < 0)
+        goto on_err;
+
+    // Build a dictionary of the device's capabilities
+    for (i = 0 ; i < EV_MAX ; i++) {
+        if (test_bit(ev_bits, i)) {
+            capability = PyLong_FromLong(i);
+            eventcodes = PyList_New(0);
+
+            memset(&code_bits, 0, sizeof(code_bits));
+            ioctl(_fd, EVIOCGBIT(i, KEY_MAX), code_bits);
+            for (j = 0; j < KEY_MAX; j++) {
+                if (test_bit(code_bits, j)) {
+                    PyList_Append(eventcodes, PyLong_FromLong(j));
+                }
+            }
+
+            PyDict_SetItem(capabilities, capability, eventcodes);
+        }
+    }
+
+    return Py_BuildValue("O", capabilities);
+
+    on_err:
+        PyErr_SetFromErrno(PyExc_IOError);
+        return NULL;
 }
 
 
@@ -130,49 +181,29 @@ event_unpack(PyObject *self, PyObject *args)
 static PyObject *
 ioctl_devinfo(PyObject *self, PyObject *args)
 {
-    int fd, i, j, nophys;
+    int fd, nophys;
+    PyObject* capabilities = NULL;
 
     struct input_id iid;
     char name[MAX_NAME_SIZE];
     char phys[MAX_NAME_SIZE];
-    char ev_mask[EV_MAX/8], key_mask[KEY_MAX/8];
-
-    // Capabilities is a mapping of supported event types to lists of handled
-    // event codes e.g: {1: [272, 273, 274, 275], 2: [0, 1, 6, 8]}
-    PyObject* capabilities = PyDict_New();
-    PyObject* eventcodes = 0;
-    PyObject* capability = 0;
 
     int ret = PyArg_ParseTuple(args, "ii", &fd, &nophys);
     if (!ret) return NULL;
 
     memset(&iid,  0, sizeof(iid));
-    memset(&ev_mask, 0, sizeof(ev_mask));
 
     if (ioctl(fd, EVIOCGID, &iid) < 0)                 goto on_err;
     if (ioctl(fd, EVIOCGNAME(sizeof(name)), name) < 0) goto on_err;
-    if (ioctl(fd, EVIOCGBIT(0, EV_MAX), ev_mask) < 0)  goto on_err;
 
-    // Build a dictionary of the device's capabilities
-    for (i = 0 ; i < EV_MAX ; i++) {
-        if (test_bit(ev_mask, i)) {
-            capability = PyLong_FromLong(i);
-            eventcodes = PyList_New(0);
-
-            memset(&key_mask, 0, sizeof(key_mask));
-            ioctl(fd, EVIOCGBIT(i, KEY_MAX), key_mask);
-            for (j = 0; j < KEY_MAX; j++)
-                if (test_bit(key_mask, j))
-                    PyList_Append(eventcodes, PyLong_FromLong(j));
-
-            PyDict_SetItem(capabilities, capability, eventcodes);
-        }
-    }
+    // Get device capabilities
+    capabilities = ioctl_capabilities(self, Py_BuildValue("(i)", fd));
 
     // Uinput devices do not have a physical topology associated with them
-    if (!nophys)
-        if (ioctl(fd, EVIOCGPHYS(sizeof(phys)), phys) < 0) goto on_err;
-    else
+    if (!nophys) {
+        if (ioctl(fd, EVIOCGPHYS(sizeof(phys)), phys) < 0)
+            goto on_err;
+    } else
         phys[0] = ' ';
 
     return Py_BuildValue("hhhhssO", iid.bustype, iid.vendor, iid.product, iid.version,
@@ -183,11 +214,13 @@ ioctl_devinfo(PyObject *self, PyObject *args)
         return NULL;
 }
 
+
 static PyMethodDef MethodTable[] = {
-    { "unpack",           event_unpack,     METH_VARARGS, "unpack a single input event" },
-    { "ioctl_devinfo",    ioctl_devinfo,    METH_VARARGS, "fetch input device info" },
-    { "device_read",      device_read,      METH_VARARGS, "read an input event from a device" },
-    { "device_read_many", device_read_many, METH_VARARGS, "read all available input events from a device" },
+    { "unpack",             event_unpack,       METH_VARARGS, "unpack a single input event" },
+    { "ioctl_devinfo",      ioctl_devinfo,      METH_VARARGS, "fetch input device info" },
+    { "ioctl_capabilities", ioctl_capabilities, METH_VARARGS, "fetch input device capabilities" },
+    { "device_read",        device_read,        METH_VARARGS, "read an input event from a device" },
+    { "device_read_many",   device_read_many,   METH_VARARGS, "read all available input events from a device" },
 
     { NULL, NULL, 0, NULL}
 };
