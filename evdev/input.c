gvalkov/python-evdev

commit 94b28e0de4f5872b9cc2ed8a6e580515760ddc65

raise IOError instead of returnin None on erronous reads

evdev/input.c
@@ -56,9 +56,9 @@ device_read(PyObject *self, PyObject *args)
 
     int n = read(fd, &event, sizeof(event));
 
-    if (n<0) {
-        Py_INCREF(Py_None);
-        return Py_None;
+    if (n < 0) {
+        PyErr_SetFromErrno(PyExc_IOError);
+        return NULL;
     }
 
     PyObject* sec  = PyLong_FromLong(event.time.tv_sec);
@@ -90,8 +90,10 @@ device_read_many(PyObject *self, PyObject *args)
     size_t event_size = sizeof(struct input_event);
     size_t nread = read(fd, event, event_size*64);
 
-    if (nread == -1)
-        return PyTuple_New(0);
+    if (nread < 0) {
+        PyErr_SetFromErrno(PyExc_IOError);
+        return NULL;
+    }
 
     // Construct a list of event tuples, which we'll make sense of in Python
     for (i = 0 ; i < nread/event_size ; i++) {

commit 3502d8f05954ec232d4a6756e84adbe472c10d6a

fix segfault in device_read_many()
device_read_many() was not detecting failed read() calls because nread
was unsigned.  Bug introduced by 94b28e0d.

evdev/input.c
@@ -88,7 +88,7 @@ device_read_many(PyObject *self, PyObject *args)
     struct input_event event[64];
 
     size_t event_size = sizeof(struct input_event);
-    size_t nread = read(fd, event, event_size*64);
+    ssize_t nread = read(fd, event, event_size*64);
 
     if (nread < 0) {
         PyErr_SetFromErrno(PyExc_IOError);