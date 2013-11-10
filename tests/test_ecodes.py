5f80cf2daaab1d3ecd7e693f3389e05123e78c80
Georgi Valkov <georgi.t.valkov@gmail.com>
2013-11-10 16:14:52 +0100

    ecodes test did not respect list values

--- tests/test_ecodes.py
+++ tests/test_ecodes.py
@@ -4,6 +4,10 @@ from evdev import ecodes
 
 prefixes = 'KEY ABS REL SW MSC LED BTN REP SND ID EV BUS SYN FF_STATUS FF'
 
+def to_tuples(l):
+    t = lambda x: tuple(x) if isinstance(x, list) else x
+    return map(t, l)
+
 def test_equality():
     keys = []
     for i in prefixes.split():
@@ -17,6 +21,6 @@ def test_access():
     assert ecodes.REL[0] == 'REL_X'
 
 def test_overlap():
-    vals_ff = set(ecodes.FF.values())
-    vals_ff_status = set(ecodes.FF_STATUS.values())
+    vals_ff = set(to_tuples(ecodes.FF.values()))
+    vals_ff_status = set(to_tuples(ecodes.FF_STATUS.values()))
     assert bool(vals_ff & vals_ff_status) == False
