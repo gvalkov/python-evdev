0815b82980163b7c5c5e918314d8949b4d2f0405
Georgi Valkov <georgi.t.valkov@gmail.com>
2016-02-07 17:58:24 +0100

    Use bumpversion to manage version bumping

--- docs/conf.py
+++ docs/conf.py
@@ -8,8 +8,6 @@ on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('..'))
-from setup import kw
 
 # Trick autodoc into running without having built the extension modules.
 if on_rtd:
@@ -61,7 +59,7 @@ copyright = u'2012-2016, Georgi Valkov'
 # built documents.
 #
 # The full version, including alpha/beta/rc tags.
-release = kw['version']
+release = '0.5.0'
 
 # The short X.Y version.
 version = release
