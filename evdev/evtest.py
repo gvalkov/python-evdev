3a0bc6f0c798cf0f7abe484f87f84056d5b22282
Georgi Valkov <georgi.t.valkov@gmail.com>
2018-03-24 00:05:46 +0100

    Fix formatting

--- evdev/evtest.py
+++ evdev/evtest.py
@@ -95,8 +95,8 @@ def select_devices(device_dir='/dev/input'):
         print(msg % device_dir, file=sys.stderr)
         sys.exit(1)
 
-    dev_format = '{0:<3} {1.fn:<20} {1.name:<35} {1.phys:<35} {1.uniq}'
-    dev_lines = [dev_format.format(n, d) for n, d in enumerate(devices)]
+    dev_format = '{0:<3} {1.fn:<20} {1.name:<35} {1.phys:<35} {1.uniq:<4}'
+    dev_lines = [dev_format.format(num, dev) for num, dev in enumerate(devices)]
 
     print('ID  {:<20} {:<35} {:<35} {}'.format('Device', 'Name', 'Phys', 'Uniq'))
     print('-' * len(max(dev_lines, key=len)))
