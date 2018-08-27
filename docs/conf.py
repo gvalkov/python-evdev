ae19a3a9d0f9637f0cb0166426a009ca42514488
Georgi Valkov <georgi.t.valkov@gmail.com>
2018-08-27 21:11:25 +0200

    Bump version: 1.0.0 -> 1.1.0

--- docs/conf.py
+++ docs/conf.py
@@ -60,7 +60,7 @@ copyright = u'2012-2016, Georgi Valkov'
 # built documents.
 #
 # The full version, including alpha/beta/rc tags.
-release = '1.0.0'
+release = '1.1.0'
 
 # The short X.Y version.
 version = release
