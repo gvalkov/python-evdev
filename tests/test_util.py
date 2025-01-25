27eb2ff11bb6b41fa0cfcff4f80d6c26d4b65742
Georgi Valkov <georgi.t.valkov@gmail.com>
2025-01-25 18:04:39 +0100

    Fix tests

--- tests/test_util.py
+++ tests/test_util.py
@@ -6,7 +6,7 @@ def test_match_ecodes_a():
     assert res == {1: [372, 418, 419, 420]}
     assert dict(util.resolve_ecodes_dict(res)) == {
         ("EV_KEY", 1): [
-            (["KEY_FULL_SCREEN", "KEY_ZOOM"], 372),
+            (("KEY_FULL_SCREEN", "KEY_ZOOM"), 372),
             ("KEY_ZOOMIN", 418),
             ("KEY_ZOOMOUT", 419),
             ("KEY_ZOOMRESET", 420),
