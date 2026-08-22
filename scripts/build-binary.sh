05d87aad34e0ef30b9ca5bf1aeaff2790597c44c
Georgi Valkov <georgi.t.valkov@gmail.com>
2026-08-22 18:50:25 +0200

    Move prebuilt wheels to manylinux_2_34

--- scripts/build-binary.sh
+++ scripts/build-binary.sh
@@ -3,10 +3,9 @@
 set -o allexport
 set -o nounset
 
-CIBW_MANYLINUX_X86_64_IMAGE="manylinux_2_28"
-CIBW_MANYLINUX_I686_IMAGE="manylinux_2_28"
+CIBW_LINUX_X86_64_IMAGE="manylinux_2_34"
+CIBW_LINUX_I686_IMAGE="manylinux_2_34"
 CIBW_CONTAINER_ENGINE="podman"
-CIBW_SKIP="cp36-*"
 CIBW_ARCHS_LINUX="auto64"
 CIBW_BEFORE_ALL_LINUX=./scripts/cibw-before.sh
 CIBW_TEST_COMMAND="python -c 'import evdev; print(evdev)'"
